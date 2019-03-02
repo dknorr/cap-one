788aa78b11451eeb7029385a5c615ae2343fe204
dknorr <dknorr01@gmail.com>
2019-03-01 23:25:25 -0500

    Nice picture cards, detail and full image modals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import SearchBar from "./Components/SearchBar.js";
-import { Card } from "antd";
+import PicCard from "./Components/PicCard.js";
 import "./App.css";
-
-//Some consts I use
-const { Meta } = Card;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      results: []
+      results: [],
+      visibleDetails: false,
+      visibleView: false
     };
   }
 
@@ -24,20 +23,8 @@ class App extends Component {
     console.log(this.state.results);
     let pics = this.state.results.map(pic => {
       if (pic.data[0].media_type == "image") {
-        let link = pic.links[0].href;
-        return (
-          <Card
-            className="Card"
-            hoverable
-            style={{ width: "16vw", margin: "1vw" }}
-            cover={<img alt="example" src={link} />}
-          >
-            <Meta
-              title={pic.data[0].title}
-              description={pic.data[0].description}
-            />
-          </Card>
-        );
+        //let link = pic.links[0].href;
+        return <PicCard pic={pic} />;
       }
     });
     return (
---
+++ src/Components/PicCard.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { Card, Modal, Button } from "antd";
+import { isNull } from "util";
+
+const { Meta } = Card;
+
+export default class PicCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleView: false,
+      visibleDetails: false
+    };
+  }
+
+  showView = () => {
+    this.setState({
+      visibleView: true
+    });
+  };
+
+  showDetails = () => {
+    this.setState({
+      visibleDetails: true
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visibleView: false,
+      visibleDetails: false
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visibleView: false,
+      visibleDetails: false
+    });
+  };
+
+  render() {
+    let link = this.props.pic.links[0].href;
+    let date = this.props.pic.data[0].date_created.substring(0, 10);
+    let secondCreator = this.props.pic.data[0].secondary_creator;
+    if (secondCreator == null) {
+      secondCreator = "N/A";
+    }
+    return (
+      <div>
+        <Card
+          className="Card"
+          hoverable
+          style={{ width: "16vw", margin: "1vw" }}
+          cover={<img alt="example" src={link} className="Thumbnail" />}
+        >
+          <Meta title={this.props.pic.data[0].title} />
+          <Button
+            className="ButtonLeft"
+            type="primary"
+            style={{ width: "6vw" }}
+            onClick={this.showView}
+          >
+            View
+          </Button>
+          <Button
+            className="ButtonRight"
+            type="secondary"
+            style={{ width: "6vw" }}
+            onClick={this.showDetails}
+          >
+            Details
+          </Button>
+        </Card>
+        <Modal
+          width="40vw"
+          title={this.props.pic.data[0].title}
+          visible={this.state.visibleView}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <img className="BigImage" alt="Image here" src={link} />
+        </Modal>
+        <Modal
+          title={this.props.pic.data[0].title}
+          visible={this.state.visibleDetails}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>
+            <strong>Full description: </strong>
+            {this.props.pic.data[0].description}
+          </p>
+          <br />
+          <p>
+            <strong>Secondary creator:</strong> {secondCreator}
+            <br />
+            <strong>Date created: </strong>
+            {date}
+          </p>
+        </Modal>
+      </div>
+    );
+  }
+}
